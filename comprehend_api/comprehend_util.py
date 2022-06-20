a627e8a56448f214a4b64aa7b9a4591b002ea6ad
Robert Wang <rwang5688@outlook.com>
2022-06-19 22:17:39 -0700

    convert json string to dict

--- comprehend_api/comprehend_util.py
+++ comprehend_api/comprehend_util.py
@@ -1,16 +1,19 @@
 import boto3
-
+import json
 
 def call_custom_comprehend_model(the_input=None,endpoint_arn=None):
     '''call the custom comprehend model that has been previously trained to classify the document according to its medical specialty type.'''
     client = boto3.client('comprehend')
 
-    print("the_input: %s" % (the_input))
+    payload = {}
+    if the_input is not None:
+        payload = json.loads(the_input)
+        print("payload: %s" % (payload))
 
     response = {}
 
-    if 'messagge' in the_input:
-        message = the_input['message']
+    if 'messagge' in payload:
+        message = payload['message']
         print ("message: %s" % message)
 
         response = client.classify_document(
@@ -25,13 +28,16 @@ def call_detect_sentiment(the_input=None,language_code=None):
     '''call the deetect_sentiment API.'''
     client = boto3.client('comprehend')
 
-    print("the_input: %s" % (the_input))
+    payload = {}
+    if the_input is not None:
+        payload = json.loads(the_input)
+        print("payload: %s" % (payload))
 
     response = {}
-    
-    if 'messagge' in the_input:
-        message = the_input['message']
-        print ("message: %s" % (message))
+
+    if 'messagge' in payload:
+        message = payload['message']
+        print ("message: %s" % message)
 
         response = client.detect_sentiment(
             Text=the_input,
