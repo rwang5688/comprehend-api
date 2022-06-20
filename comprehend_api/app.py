a47e21243f75fc157f42233cb0cb5eea20461aef
Robert Wang <rwang5688@outlook.com>
2022-06-19 20:45:50 -0700

    update function response

--- comprehend_api/app.py
+++ comprehend_api/app.py
@@ -46,7 +46,6 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "response": response,
-            # "location": ip.text.replace("\n", "")
+            "response": response
         }),
     }
