d6905b522dd34c90d1cf76354798df4606091e1c
Robert Wang <rwang5688@outlook.com>
2022-06-19 22:06:22 -0700

    print what the_input looks like

--- comprehend_api/comprehend_util.py
+++ comprehend_api/comprehend_util.py
@@ -5,6 +5,8 @@ def call_custom_comprehend_model(the_input=None,endpoint_arn=None):
     '''call the custom comprehend model that has been previously trained to classify the document according to its medical specialty type.'''
     client = boto3.client('comprehend')
 
+    print("the_input: %s" % (the_input))
+
     response = {}
 
     if 'messagge' in the_input:
@@ -23,11 +25,13 @@ def call_detect_sentiment(the_input=None,language_code=None):
     '''call the deetect_sentiment API.'''
     client = boto3.client('comprehend')
 
+    print("the_input: %s" % (the_input))
+
     response = {}
     
     if 'messagge' in the_input:
         message = the_input['message']
-        print ("message: %s" % message)
+        print ("message: %s" % (message))
 
         response = client.detect_sentiment(
             Text=the_input,
