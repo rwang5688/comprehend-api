c91acd024ce3d8ccbabacb6dc5af12b583488c54
EC2 Default User <ec2-user@ip-10-1-29-62.us-west-2.compute.internal>
2022-06-20 05:23:09 +0000

    fix typo 'message'

--- comprehend_api/comprehend_util.py
+++ comprehend_api/comprehend_util.py
@@ -5,15 +5,12 @@ def call_custom_comprehend_model(the_input=None,endpoint_arn=None):
     '''call the custom comprehend model that has been previously trained to classify the document according to its medical specialty type.'''
     client = boto3.client('comprehend')
 
-    payload = {}
-    if the_input is not None:
-        payload = json.loads(the_input)
-        print("payload: %s" % (payload))
+    print("the_input: %s" % (the_input))
 
     response = {}
 
-    if 'messagge' in payload:
-        message = payload['message']
+    if 'message' in the_input:
+        message = the_input['message']
         print ("message: %s" % message)
 
         response = client.classify_document(
@@ -28,15 +25,12 @@ def call_detect_sentiment(the_input=None,language_code=None):
     '''call the deetect_sentiment API.'''
     client = boto3.client('comprehend')
 
-    payload = {}
-    if the_input is not None:
-        payload = json.loads(the_input)
-        print("payload: %s" % (payload))
+    print("the_input: %s" % (the_input))
 
     response = {}
 
-    if 'messagge' in payload:
-        message = payload['message']
+    if 'message' in the_input:
+        message = the_input['message']
         print ("message: %s" % message)
 
         response = client.detect_sentiment(
