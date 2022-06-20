56059c50c0c8cd248a54ccfa3453e75591515922
Robert Wang <rwang5688@outlook.com>
2022-06-19 21:51:40 -0700

    add call to detect_sentiment

--- comprehend_api/app.py
+++ comprehend_api/app.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 
 import boto3
+import comprehend_util
 import json
 
 print('Loading function')
@@ -31,6 +32,7 @@ def lambda_handler(event, context):
         'update': lambda x: dynamo.update_item(**x),
         'delete': lambda x: dynamo.delete_item(**x),
         'list': lambda x: dynamo.scan(**x),
+        'detect': lambda x: comprehend_util.call_detect_sentiment(the_input=x, language_code='en'),
         'echo': lambda x: x,
         'ping': lambda x: 'pong'
     }
---
+++ comprehend_api/comprehend_util.py
@@ -0,0 +1,38 @@
+import boto3
+
+
+def call_custom_comprehend_model(the_input=None,endpoint_arn=None):
+    '''call the custom comprehend model that has been previously trained to classify the document according to its medical specialty type.'''
+    client = boto3.client('comprehend')
+
+    response = {}
+
+    if 'messagge' in the_input:
+        message = the_input['message']
+        print ("message: %s" % message)
+
+        response = client.classify_document(
+            Text=the_input,
+            EndpointArn=endpoint_arn
+            )
+    
+    return(response)
+
+
+def call_detect_sentiment(the_input=None,language_code=None):
+    '''call the deetect_sentiment API.'''
+    client = boto3.client('comprehend')
+
+    response = {}
+    
+    if 'messagge' in the_input:
+        message = the_input['message']
+        print ("message: %s" % message)
+
+        response = client.detect_sentiment(
+            Text=the_input,
+            LanguageCode=language_code
+            )
+    
+    return(response)
+
